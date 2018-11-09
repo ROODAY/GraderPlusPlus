ed20e2064ed6c8962d2774726f6022d11367ddf4
zzxiang8752 <31775919+zzxiang8752@users.noreply.github.com>
2018-11-09 13:49:34 -0500

    create basic class

---
+++ src/grader/Student.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class Student {
+	private StudentInfo studentinfo;
+	private List<StudentAssignment> assignments;
+	public void getStudentInfo() {}
+	public void getAssignmentScore() {}
+}
