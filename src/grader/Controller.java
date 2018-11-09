f52022d9e3ce8fb5f5bba0e43b1419f097497511
Rudhra Raveendran <rudytheninja@gmail.com>
2018-11-09 14:38:32 -0500

    got basic layout

--- src/grader/Controller.java
+++ src/grader/Controller.java
@@ -1,4 +1,12 @@
 package grader;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+
 public class Controller {
+    public Label helloWorld;
+
+    public void sayHellowWorld(ActionEvent actionEvent) {
+        helloWorld.setText("Hello World!");
+    }
 }
