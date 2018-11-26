c4be8a7678729d00d578d5642beffd39c83480e1
Rudhra Raveendran <rudytheninja@gmail.com>
2018-11-25 19:47:45 -0500

    added basic sidebar functionality

--- src/grader/Controller.java
+++ src/grader/Controller.java
@@ -1,12 +1,30 @@
 package grader;
 
-import javafx.event.ActionEvent;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextInputDialog;
+import javafx.scene.control.TitledPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+
+import java.util.Optional;
 
 public class Controller {
-    public Label helloWorld;
+    public Pane sidebar;
+    public VBox semesterContainer;
 
-    public void sayHellowWorld(ActionEvent actionEvent) {
-        helloWorld.setText("Hello World!");
+    public void createNewSemester() {
+        TextInputDialog dialog = new TextInputDialog("2018/Fall");
+        dialog.setTitle("Add New Semester");
+        dialog.setContentText("Please enter the semester:");
+        Optional<String> result = dialog.showAndWait();
+        result.ifPresent(semester -> {
+            TitledPane semesterPane = new TitledPane(semester, new Button("Add Class"));
+            semesterPane.setExpanded(false);
+            semesterPane.setMinWidth(semesterContainer.getWidth());
+            semesterContainer.widthProperty().addListener((obs, oldScene, newScene) -> {
+                semesterPane.setMinWidth(semesterContainer.getWidth());
+            });
+            semesterContainer.getChildren().add(semesterPane);
+        });
     }
 }
--- src/grader/Main.java
+++ src/grader/Main.java
@@ -4,17 +4,21 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.SplitPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("scene.fxml"));
         primaryStage.setTitle("Grader++");
         primaryStage.setScene(new Scene(root));
         primaryStage.setMaximized(true);
         primaryStage.show();
+
+        SplitPane splitPane = (SplitPane) primaryStage.getScene().lookup("#splitPane");
+        splitPane.setDividerPositions(0.15);
     }
 
 
