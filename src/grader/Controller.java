746c687a1328e7094925e6c2d930fe209f4f4bfb
Rudhra Raveendran <rudytheninja@gmail.com>
2018-12-02 19:17:12 -0500

    save state

--- src/grader/Controller.java
+++ src/grader/Controller.java
@@ -3,6 +3,7 @@ package grader;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextInputDialog;
 import javafx.scene.control.TitledPane;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 
@@ -18,13 +19,69 @@ public class Controller {
         dialog.setContentText("Please enter the semester:");
         Optional<String> result = dialog.showAndWait();
         result.ifPresent(semester -> {
-            TitledPane semesterPane = new TitledPane(semester, new Button("Add Class"));
+            Button addButton = new Button("Add Class");
+            VBox tpVBox = new VBox(addButton);
+            AnchorPane tpLayout = new AnchorPane(tpVBox);
+            AnchorPane.setTopAnchor(tpVBox, 0.0);
+            AnchorPane.setRightAnchor(tpVBox, 0.0);
+            AnchorPane.setBottomAnchor(tpVBox, 0.0);
+            AnchorPane.setLeftAnchor(tpVBox, 20.0);
+            TitledPane semesterPane = new TitledPane(semester, tpLayout);
+
+            addButton.setOnAction(action -> createNewClass(tpVBox));
+
             semesterPane.setExpanded(false);
             semesterPane.setMinWidth(semesterContainer.getWidth());
-            semesterContainer.widthProperty().addListener((obs, oldScene, newScene) -> {
-                semesterPane.setMinWidth(semesterContainer.getWidth());
-            });
+
+            semesterContainer.widthProperty().addListener((obs, oldScene, newScene) -> semesterPane.setMinWidth(semesterContainer.getWidth()));
             semesterContainer.getChildren().add(semesterPane);
+        });
+    }
+
+    private void createNewClass(VBox vbox) {
+        TextInputDialog dialog = new TextInputDialog("CS 591");
+        dialog.setTitle("Add New Class");
+        dialog.setContentText("Please enter the Class:");
+        Optional<String> result = dialog.showAndWait();
+        result.ifPresent(classname -> {
+            Button addButton = new Button("Add Section");
+            VBox tpVBox = new VBox(addButton);
+            AnchorPane tpLayout = new AnchorPane(tpVBox);
+            AnchorPane.setTopAnchor(tpVBox, 0.0);
+            AnchorPane.setRightAnchor(tpVBox, 0.0);
+            AnchorPane.setBottomAnchor(tpVBox, 0.0);
+            AnchorPane.setLeftAnchor(tpVBox, 20.0);
+            TitledPane classPane = new TitledPane(classname, tpLayout);
+
+            addButton.setOnAction(action -> createNewSection(tpVBox));
+
+            classPane.setExpanded(false);
+            classPane.setMinWidth(semesterContainer.getWidth());
+            semesterContainer.widthProperty().addListener((obs, oldScene, newScene) -> classPane.setMinWidth(semesterContainer.getWidth()));
+            int index = vbox.getChildren().size() - 1;
+            vbox.getChildren().add(index, classPane);
+        });
+    }
+
+    private void createNewSection(VBox vbox) {
+        TextInputDialog dialog = new TextInputDialog("A1");
+        dialog.setTitle("Add New Section");
+        dialog.setContentText("Please enter the section:");
+        Optional<String> result = dialog.showAndWait();
+        result.ifPresent(section -> {
+            VBox tpVBox = new VBox();
+            AnchorPane tpLayout = new AnchorPane(tpVBox);
+            AnchorPane.setTopAnchor(tpVBox, 0.0);
+            AnchorPane.setRightAnchor(tpVBox, 0.0);
+            AnchorPane.setBottomAnchor(tpVBox, 0.0);
+            AnchorPane.setLeftAnchor(tpVBox, 20.0);
+            TitledPane sectionPane = new TitledPane(section, tpLayout);
+
+            sectionPane.setExpanded(false);
+            sectionPane.setMinWidth(semesterContainer.getWidth());
+            semesterContainer.widthProperty().addListener((obs, oldScene, newScene) -> sectionPane.setMinWidth(semesterContainer.getWidth()));
+            int index = vbox.getChildren().size() - 1;
+            vbox.getChildren().add(index, sectionPane);
         });
     }
 }
