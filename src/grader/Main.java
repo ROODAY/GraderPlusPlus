b0ca76811ca3b3f6fbdf2a95ba36461ba6df1ffe
Miguel <>
2018-12-05 15:29:24 -0500

    Edited Main.java back to normal

--- src/grader/Main.java
+++ src/grader/Main.java
@@ -7,31 +7,27 @@ import javafx.scene.Scene;
 import javafx.scene.control.SplitPane;
 import javafx.stage.Stage;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-//        Parent root = FXMLLoader.load(getClass().getResource("scene.fxml"));
-//        primaryStage.setTitle("Grader++");
-//        primaryStage.setScene(new Scene(root));
-//        primaryStage.setMaximized(true);
-//        primaryStage.show();
-//
-//        SplitPane splitPane = (SplitPane) primaryStage.getScene().lookup("#splitPane");
-//        splitPane.setDividerPositions(0.15);
+        Parent root = FXMLLoader.load(getClass().getResource("scene.fxml"));
+        primaryStage.setTitle("Grader++");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setMaximized(true);
+        primaryStage.show();
+
+        SplitPane splitPane = (SplitPane) primaryStage.getScene().lookup("#splitPane");
+        splitPane.setDividerPositions(0.15);
     }
 
 
     public static void main(String[] args) {
-        //launch(args);
+        launch(args);
         
         StudentClass student = new StudentClass();
         
-        //student.create(1345, "John", "Smith", "john@bu.edu", "Undergrad");
         
         student.uploadStudentsCSV("/Users/miguelvaldez/Desktop/students_roster.csv");
       
