cd267584b0177853b3d744d89da6bb6e01561d24
Rudhra Raveendran <rudytheninja@gmail.com>
2018-12-06 12:41:22 -0500

    save state for splash

--- src/grader/Controller.java
+++ src/grader/Controller.java
@@ -1,24 +1,4 @@
 package grader;
 
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import entity.Course;
-import java.awt.event.ActionEvent;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import javafx.scene.control.Button;
-import javafx.scene.control.Control;
-import javafx.scene.control.TextInputDialog;
-import javafx.scene.control.TitledPane;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
-
-import java.io.IOException;
-import java.lang.reflect.Method;
-import java.util.Optional;
-
 public class Controller {
 }
--- src/grader/Main.java
+++ src/grader/Main.java
@@ -1,36 +1,57 @@
 package grader;
 
+import javafx.animation.FadeTransition;
 import javafx.application.Application;
 import javafx.beans.value.ChangeListener;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.SplitPane;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 import model.StudentClass;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        StudentClass.create("Jane Doe", "jane@bu.edu", 4.0);
+        //StudentClass.create("Jane Doe", "jane@bu.edu", 4.0);
         primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/assets/logo.png")));
 
-        Parent root = FXMLLoader.load(getClass().getResource("scene.fxml"));
+        Parent splash = FXMLLoader.load(getClass().getResource("splashScreen.fxml"));
+        Parent home = FXMLLoader.load(getClass().getResource("homeScreen.fxml"));
         primaryStage.setTitle("GradeBook");
-        primaryStage.setScene(new Scene(root));
-        primaryStage.setMaximized(true);
+        primaryStage.setScene(new Scene(splash));
+        //primaryStage.setMaximized(true);
         primaryStage.show();
 
-        SplitPane splitPane = (SplitPane) primaryStage.getScene().lookup("#splitPane");
-        splitPane.setDividerPositions(0.15);
+        FadeTransition ft = new FadeTransition(Duration.millis(2000), splash);
+        ft.setFromValue(1.0);
+        ft.setToValue(0.0);
+        ft.play();
 
-        ChangeListener<Number> stageSizeListener = (observable, oldValue, newValue) ->
-                splitPane.setDividerPositions(0.15);
+        ft.setOnFinished(event -> {
+            FadeTransition ft2 = new FadeTransition(Duration.millis(1000), home);
+            ft2.setFromValue(0.0);
+            ft2.setToValue(1.0);
+            ft2.play();
+            primaryStage.setScene(new Scene(home));
+            primaryStage.setMaximized(true);
 
-        primaryStage.widthProperty().addListener(stageSizeListener);
-        primaryStage.heightProperty().addListener(stageSizeListener);
+
+            SplitPane splitPane = (SplitPane) primaryStage.getScene().lookup("#splitPane");
+            splitPane.setDividerPositions(0.15);
+
+            ChangeListener<Number> stageSizeListener = (observable, oldValue, newValue) ->
+                    splitPane.setDividerPositions(0.15);
+
+            primaryStage.widthProperty().addListener(stageSizeListener);
+            primaryStage.heightProperty().addListener(stageSizeListener);
+        });
+
     }
 
 
